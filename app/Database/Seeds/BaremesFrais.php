7227539bc60486156359c740bb2506d37beeb54f
LovaPinto <lovasoa.alexis@gmail.com>
2026-07-20 14:18:22 +0300

    add retrait et depost

--- app/Database/Seeds/BaremesFrais.php
+++ app/Database/Seeds/BaremesFrais.php
@@ -8,6 +8,18 @@ class BaremesFrais extends Seeder
 {
     public function run()
     {
-        //
+        $now = date('Y-m-d H:i:s');
+
+        $data = [
+            ['type_operation_id' => 2, 'montant_min' => 100,    'montant_max' => 1000,    'frais' => 50,   'actif' => 1, 'date_debut' => $now, 'date_fin' => null],
+            ['type_operation_id' => 2, 'montant_min' => 1001,   'montant_max' => 5000,    'frais' => 50,   'actif' => 1, 'date_debut' => $now, 'date_fin' => null],
+            ['type_operation_id' => 2, 'montant_min' => 5001,   'montant_max' => 10000,   'frais' => 100,  'actif' => 1, 'date_debut' => $now, 'date_fin' => null],
+            ['type_operation_id' => 2, 'montant_min' => 10001,  'montant_max' => 25000,   'frais' => 200,  'actif' => 1, 'date_debut' => $now, 'date_fin' => null],
+            ['type_operation_id' => 2, 'montant_min' => 25001,  'montant_max' => 50000,   'frais' => 400,  'actif' => 1, 'date_debut' => $now, 'date_fin' => null],
+            ['type_operation_id' => 3, 'montant_min' => 100,    'montant_max' => 10000,   'frais' => 100,  'actif' => 1, 'date_debut' => $now, 'date_fin' => null],
+            ['type_operation_id' => 3, 'montant_min' => 10001,  'montant_max' => 50000,   'frais' => 300,  'actif' => 1, 'date_debut' => $now, 'date_fin' => null],
+        ];
+
+        $this->db->table('baremes_frais')->insertBatch($data);
     }
 }
